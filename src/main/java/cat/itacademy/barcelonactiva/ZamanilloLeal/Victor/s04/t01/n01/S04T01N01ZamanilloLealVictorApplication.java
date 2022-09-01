db7e638c86bcfd292f2ca59cf9c996367d00ec85
nillozama <nillozama@gmail.com>
2022-09-01 11:33:39 +0200

    version 1

---
+++ src/main/java/cat/itacademy/barcelonactiva/ZamanilloLeal/Victor/s04/t01/n01/S04T01N01ZamanilloLealVictorApplication.java
@@ -0,0 +1,17 @@
+package cat.itacademy.barcelonactiva.ZamanilloLeal.Victor.s04.t01.n01;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01ZamanilloLealVictorApplication {
+	
+	@Autowired
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01ZamanilloLealVictorApplication.class, args);
+	}
+
+}
